6db6338a3368e3023e8b9fbcdf23f1e08d5d5b18
Nick Myalik <nick.myalik@hqo.co>
2022-07-08 03:21:06 +0300

    Add domain cert and record

--- terraform/monolith/domain.tf
+++ terraform/monolith/domain.tf
@@ -22,3 +22,25 @@ resource "aws_acm_certificate" "cert" {
     create_before_destroy = true
   }
 }
+
+resource "aws_route53_record" "cert_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      record  = dvo.resource_record_value
+      type    = dvo.resource_record_type
+      zone_id = aws_route53_zone.root_zone.zone_id
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
+  type    = each.value.type
+  zone_id = each.value.zone_id
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_record : record.fqdn]
+}
